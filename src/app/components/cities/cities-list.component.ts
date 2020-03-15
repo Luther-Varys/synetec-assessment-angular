a19d271556695a3f54d73e92931270198f54a187
Carlo Bramini <braminicarlo@live.com>
2020-03-15 15:42:18 +0000

    Displaying the cities in the table and added functionality of the delete button. Modified the cities-list.component and view. The delete city call is working but it's implemented incorrectly in the web.api

--- src/app/components/cities/cities-list.component.ts
+++ src/app/components/cities/cities-list.component.ts
@@ -1,17 +1,70 @@
 import { Component, OnInit } from "@angular/core";
 import { ICity } from "../../models/city.model";
+import { CitiesService } from "../../services/cities/cities.service";
+import { Observable } from "rxjs";
+import { BaseService } from "../../services/base.service";
+import { CitiesEndpoint } from "../../services/cities/cities-endpoint.service";
+import { concat } from "rxjs/observable/concat";
+import { flatMap } from "rxjs/operators";
 
 @Component ({
     selector: 'cities-list',
     templateUrl: './cities-list.component.html',
-    styleUrls: ['./cities-list.component.css']
+    styleUrls: ['./cities-list.component.css'],
 })
 
 export class CitiesListComponent implements OnInit{
 
-    cities: ICity[];
-    constructor() {}
+    cities: ICity[] = [];
+    constructor(private _citiesService: CitiesService) {}
 
     ngOnInit(): void {
+        const obser:Observable<ICity[]> = this._citiesService.getCities();
+        obser.subscribe(
+            (vm: ICity[])=>{
+                // this.cities = vm;
+                if(vm === null){
+                    this.cities = [];
+                }
+                else{
+                    this.cities = vm;
+                    console.log("zr: city: getCities", vm[0]);
+                }
+                
+                },
+            (error:any)=>{
+                console.log("ZR erros getCities:", error);
+            },
+            ()=>{
+                console.log("ZR oservable completed. getCities");
+            }
+        );
     }
+
+
+    //events
+    onBtnClickDelete(cityId:number){
+        console.log("zr: onBtnClickDelete number", cityId);
+        this._citiesService.deleteCity(cityId)
+        .pipe(
+            flatMap((resp: any) => this._citiesService.getCities()),            
+        )
+        .subscribe(
+            (vm: ICity[])=>{
+                if(vm === null){
+                    this.cities = [];
+                }
+                else{
+                    this.cities = vm;
+                    console.log("zr: city: getCities", vm);
+                }
+            },
+            (error:any)=>{
+            },
+            ()=>{
+                console.log("ZR oservable completed. getCities");
+            }
+        );
+    }
+    
 }
